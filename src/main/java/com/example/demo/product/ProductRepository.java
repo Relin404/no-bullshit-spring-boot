4411af25d6d4b1eba40e8a5639052c63200ef38b
relin404 <ahmadmashaal01@gmail.com>
2025-04-03 02:37:41 +0200

    feat: add custom query ProductRepository for improved search functionality
    Signed-off-by: relin404 <ahmadmashaal01@gmail.com>

--- src/main/java/com/example/demo/product/ProductRepository.java
+++ src/main/java/com/example/demo/product/ProductRepository.java
@@ -2,8 +2,16 @@ package com.example.demo.product;
 
 import com.example.demo.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  List<Product> findByNameContaining(String name);
+
+  @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+  List<Product> findByNameOrDescriptionContaining(@Param("keyword") String name);
 }
