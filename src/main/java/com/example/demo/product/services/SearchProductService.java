c20ff0df24bf8d984a0312c76c6775dabd01cbb1
relin404 <ahmadmashaal01@gmail.com>
2025-04-03 02:38:12 +0200

    `feat: implement product search service with name and description filtering`
    Signed-off-by: relin404 <ahmadmashaal01@gmail.com>

--- src/main/java/com/example/demo/product/services/SearchProductService.java
+++ src/main/java/com/example/demo/product/services/SearchProductService.java
@@ -1,4 +1,25 @@
 package com.example.demo.product.services;
 
-public class SearchProductService {
+import com.example.demo.Query;
+import com.example.demo.product.ProductRepository;
+import com.example.demo.product.model.ProductDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchProductService implements Query<String, List<ProductDTO>> {
+  private final ProductRepository productRepository;
+
+  public SearchProductService(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
+
+  @Override
+  public ResponseEntity<List<ProductDTO>> execute(String name) {
+    return ResponseEntity.status(HttpStatus.OK).body(productRepository.findByNameOrDescriptionContaining(name).stream().map(
+            ProductDTO::new).toList());
+  }
 }
