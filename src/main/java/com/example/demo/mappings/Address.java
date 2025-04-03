8c38b7f66b78d494dd3bad27ed1a6b2c4ad32723
relin404 <ahmadmashaal01@gmail.com>
2025-04-03 02:59:13 +0200

    `feat: add Address entity
    Signed-off-by: relin404 <ahmadmashaal01@gmail.com>

---
+++ src/main/java/com/example/demo/mappings/Address.java
@@ -0,0 +1,23 @@
+package com.example.demo.mappings;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "address")
+public class Address {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Integer id;
+
+  @Column(name = "street")
+  private String street;
+
+  @Column(name = "city")
+  private String city;
+
+  @Column(name = "state")
+  private String state;
+}
