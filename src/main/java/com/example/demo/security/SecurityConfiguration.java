efc708485cb6e52f4ec20d835dfae035ed378c6a
relin404 <ahmadmashaal01@gmail.com>
2025-04-06 02:59:22 +0200

    feat: add SecurityConfiguration for user authentication and authorization
    Signed-off-by: relin404 <ahmadmashaal01@gmail.com>

---
+++ src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -0,0 +1,60 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+  @Bean
+  public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+    UserDetails admin = User
+      .withUsername("admin")
+//      .authorities("BASIC", "SPECIAL")
+      .roles("superuser")
+      .password(encoder.encode("123"))
+      .build();
+
+    UserDetails user = User
+      .withUsername("user")
+//      .authorities("BASIC")
+      .roles("basicuser")
+      .password(encoder.encode("123"))
+      .build();
+
+    return new InMemoryUserDetailsManager(admin, user);
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+      .csrf(AbstractHttpConfigurer::disable)
+      .authorizeHttpRequests(authorize -> {
+        authorize.anyRequest().authenticated();
+//        authorize.requestMatchers("/security/open").permitAll();
+//        authorize.requestMatchers("/security/closed").authenticated();
+//        authorize.requestMatchers(HttpMethod.POST, "/products").authenticated();
+
+//        authorize.requestMatchers(HttpMethod.GET, "security/special").hasAuthority("SPECIAL");
+//        authorize.requestMatchers(HttpMethod.GET, "/security/basic").hasAnyAuthority("BASIC", "SPECIAL");
+      })
+      .httpBasic(Customizer.withDefaults())
+      .build();
+  }
+}
