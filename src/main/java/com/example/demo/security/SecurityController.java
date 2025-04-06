efaea1a0b42093db7b86cc4cee815d348eab3652
relin404 <ahmadmashaal01@gmail.com>
2025-04-06 03:33:07 +0200

    feat: implement createUser controller
    Signed-off-by: relin404 <ahmadmashaal01@gmail.com>

--- src/main/java/com/example/demo/security/SecurityController.java
+++ src/main/java/com/example/demo/security/SecurityController.java
@@ -1,13 +1,27 @@
 package com.example.demo.security;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/security")
 public class SecurityController {
+  private final CreateNewUserService createNewUserService;
+
+  public SecurityController(CreateNewUserService createNewUserService) {
+    this.createNewUserService = createNewUserService;
+  }
+
+  @PostMapping("/users")
+  public ResponseEntity<String> createUser(CustomUser user) {
+    // Logic to create a new user
+    return createNewUserService.execute(user);
+  }
+
   @GetMapping("/open")
   public String open() {
     return "This is an open endpoint";
@@ -23,7 +37,6 @@ public class SecurityController {
   public String special() {
     return "This is a special endpoint";
   }
-
 
   @PreAuthorize("hasRole('superuser') or hasRole('basicuser')")
   @GetMapping("/basic")
