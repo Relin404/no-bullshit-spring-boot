4090de7833b3488bf4bb2896b217526ab20a83a9
relin404 <ahmadmashaal01@gmail.com>
2025-04-11 18:30:35 +0200

    feat: enhance CustomUser entity with validation and constructors
    Signed-off-by: relin404 <ahmadmashaal01@gmail.com>

--- src/main/java/com/example/demo/security/CustomUser.java
+++ src/main/java/com/example/demo/security/CustomUser.java
@@ -4,17 +4,24 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "custom_user")
 public class CustomUser {
   @Id
-  @Column
+  @Column(name = "username")
+  @NotNull
   private String username;
 
-  @Column
+  @Column(name = "password")
+  @NotNull
   private String password;
 }
 
